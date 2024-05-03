597ee00e9ccfed9fe552a4d0aa436c827190f4f7
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-03 23:12:57 +0600

    add category view is done

--- blogproject/categories/views.py
+++ blogproject/categories/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from categories.forms import CategoryForm
 
 # Create your views here.
+def addCategory(request):
+    if request.method=='POST':
+        category = CategoryForm(request.POST)
+        if category.is_valid():
+            category.save()
+            return redirect('addCategory')
+    else:
+        category = CategoryForm()
+    return render(request,'add_categories.html',{'form':category})
