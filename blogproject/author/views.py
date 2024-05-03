96b737e239929c31e9a9030fe3d53d65c7520b5d
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-03 22:46:17 +0600

    add author is done

--- blogproject/author/views.py
+++ blogproject/author/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import forms
 # Create your views here.
 def addAuthor(request):
-    author_form  = forms.AuthorForm()
+    if request.method=='POST':
+        author_form  = forms.AuthorForm(request.POST)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect('addAuthor')
+    else:
+        author_form  = forms.AuthorForm(request.POST)
     return render(request,'add_author.html',{'form':author_form})
