31dbdcf46030077bdee2158327a39ec00ac318b3
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-05-03 23:05:06 +0600

    add profile is done

--- blogproject/profiles/forms.py
+++ blogproject/profiles/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from profiles.models import Profile
 
-class AddProfile(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
--- blogproject/profiles/views.py
+++ blogproject/profiles/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from profiles.forms import ProfileForm
+
 
 # Create your views here.
 def addProfile(request):
-    return render(request,'add_profile.html')
+    if request.method=='POST':
+        author_profile = ProfileForm(request.POST)
+        if author_profile.is_valid():
+            author_profile.save()
+            return redirect('addProfile')
+    else:
+        author_profile= ProfileForm()
+    return render(request,'add_profile.html',{'form':author_profile})
